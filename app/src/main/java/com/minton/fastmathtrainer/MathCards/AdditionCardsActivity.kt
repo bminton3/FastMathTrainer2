ca5ebebe307f6f62af59bc7035014eeb9f971ac5
bminton3 <ben.minton@gmail.com>
2019-08-12 00:48:08 -0400

    Migrated to androidx

--- app/src/main/java/com/minton/fastmathtrainer/MathCards/AdditionCardsActivity.kt
+++ app/src/main/java/com/minton/fastmathtrainer/MathCards/AdditionCardsActivity.kt
@@ -1,6 +1,6 @@
 package com.minton.fastmathtrainer.MathCards
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.minton.fastmathtrainer.R
